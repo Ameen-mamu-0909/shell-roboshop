84496f93f198bfbbc0f6e8f14caec8a79923e045
Ameen <your-email@example.com>
2026-05-08 10:55:55 +0530

    mamu

--- 01.loop.sh
+++ 01.loop.sh
@@ -5,13 +5,25 @@ SG_ID="sg-09f13b2fb5f2c245b"
 
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=mamu}]' --query 'Instances[0].InstanceId' --output tex)
+    INSTANCE_ID=$(aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type t3.micro \
+    --security-group-ids $SG_ID \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=mamu}]' \
+    --query 'Instances[0].InstanceId' \
+    --output text)
 
-    if   [ $instance != "frontend" ]; then
-         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations [0].Instances[0].PrivateIpAddress' --output text)
+    if [ $instance != "frontend" ]; then
+         IP=$(aws ec2 describe-instances \
+         --instance-ids $INSTANCE_ID \
+         --query 'Reservations[0].Instances[0].PrivateIpAddress' \
+         --output text)
     else
-      IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations [0].Instances[0].PublicIpAddress' --output text)
+         IP=$(aws ec2 describe-instances \
+         --instance-ids $INSTANCE_ID \
+         --query 'Reservations[0].Instances[0].PublicIpAddress' \
+         --output text)
     fi
 
-   echo "$instance: $IP"
+    echo "$instance: $IP"
 done
